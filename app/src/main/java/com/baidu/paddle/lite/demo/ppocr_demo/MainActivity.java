b5a6380a41fcf1c51625cf7ccaedaa6e6e6fd894
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2022-07-06 14:45:18 +1000

    change model name

--- app/src/main/java/com/baidu/paddle/lite/demo/ppocr_demo/MainActivity.java
+++ app/src/main/java/com/baidu/paddle/lite/demo/ppocr_demo/MainActivity.java
@@ -6,7 +6,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -26,11 +25,9 @@ import com.baidu.paddle.lite.demo.util.Constants;
 import com.baidu.paddle.lite.demo.util.IdCardUtil;
 
 import java.io.File;
-import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 
 
 public class MainActivity extends Activity implements View.OnClickListener, CameraSurfaceView.OnTextureChangedListener {
@@ -46,9 +43,9 @@ public class MainActivity extends Activity implements View.OnClickListener, Came
     long lastFrameTime;
 
     // Model settings of object detection
-    protected String detModelPath = "ch_ppocr_mobile_v2.0_det_slim_opt_original.nb";
-    protected String recModelPath = "ch_ppocr_mobile_v2.0_rec_slim_opt_original.nb";
-    protected String clsModelPath = "ch_ppocr_mobile_v2.0_cls_slim_opt_original.nb";
+    protected String detModelPath = "ch_ppocr_mobile_v2.0_det_slim_opt.nb";
+    protected String recModelPath = "ch_ppocr_mobile_v2.0_rec_slim_opt.nb";
+    protected String clsModelPath = "ch_ppocr_mobile_v2.0_cls_slim_opt.nb";
     protected String labelPath = "ppocr_keys_v1.txt";
     protected String nationPath = "nation.txt";
 
